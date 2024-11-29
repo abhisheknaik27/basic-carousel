fda553df59303456f42fc3e3fb83f6380f32f9cf
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-29 20:00:10 +0530

    star rating - react icon

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,30 +1,47 @@
 import React, { useState } from "react";
-import { CiStar } from "react-icons/ci";
-{
-  /* <CiStar /> */
-}
+import { FaStar } from "react-icons/fa";
 
 const StarRating = ({ starCount }) => {
   const [clicked, setClicked] = useState();
   const [hover, setHover] = useState();
-
+  const [hover2, setHover2] = useState();
   return (
-    <div className="flex h-screen w-full justify-center items-center text-5xl ">
-      {new Array(starCount).fill(0).map((value, index) => (
-        <span
-          key={index}
-          onClick={() => setClicked(index + 1)}
-          className={
-            (hover === 0 && index < clicked) || index < hover
-              ? "text-orange-500"
-              : ""
-          }
-          onMouseEnter={() => setHover(index + 1)}
-          onMouseLeave={() => setHover(0)}
-        >
-          &#9733;
-        </span>
-      ))}
+    <div className="flex flex-col gap-10 h-screen w-full justify-center items-center">
+      <div className="text-5xl ">
+        {new Array(starCount).fill(0).map((value, index) => (
+          <span
+            key={index}
+            onClick={() => setClicked(index + 1)}
+            className={
+              (hover === 0 && index < clicked) || index < hover
+                ? "text-orange-500"
+                : ""
+            }
+            onMouseEnter={() => setHover(index + 1)}
+            onMouseLeave={() => setHover(0)}
+          >
+            &#9733;
+          </span>
+        ))}
+      </div>
+
+      <div className="flex text-4xl ">
+        {new Array(starCount).fill(0).map((val, index) => (
+          <span
+            key={index}
+            onClick={() => setClicked(index + 1)}
+            onMouseEnter={() => setHover2(index + 1)}
+            onMouseLeave={() => setHover2(0)}
+            className={
+              (hover2 === 0 && index < clicked) || index < hover2
+                ? "text-yellow-500"
+                : ""
+            }
+          >
+            <FaStar />
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
